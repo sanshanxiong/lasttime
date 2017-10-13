e675a6aae407a24e29a6234ce02660305f00efa9
sahnshanxiong <413748250@qq.com>
2017-10-13 09:24:43 +0800

    modified moban

---
+++ admin/home/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qcx
+ * Date: 2017/10/13
+ * Time: 8:21
+ */
+require_once "../../start.php";
+require_once  ROOT_PATH."lib/MyPDO.class.php";
+require_once ROOT_PATH."view/admin/home/index.html";
--- start.php
+++ start.php
@@ -5,8 +5,14 @@
  * Date: 2017/10/12
  * Time: 20:03
  */
-define('ROOT_PATH',__DIR__.'/');
+/*
+ *getcwd() 获取的是工作目录的路径，
+ *    即a文件若被b包含，执行b时返回的是b的路径；a文件若被c包含，执行c时，返回的是c 的路径
+*/
+//start.php
+define('ROOT_PATH',__DIR__.'/');//_DIR_获取的是它所处于的原始文件的路径。
 include_once ROOT_PATH."config/config.php";
 define("PUBLIC_PATH",ROOT_PATH."public/");
 define("UPLOAD_PATH",PUBLIC_PATH."upload/");
-define("STATIC_PATH","../../");
+
+//echo getcwd()."<br/>";
